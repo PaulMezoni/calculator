dd22786bd29be8c87fa8160694d00f77c0719402
LeonidAlekseenko <73501360+LeonidAlekseenko@users.noreply.github.com>
2020-11-01 18:58:28 +0300

    Update program.java

--- program.java
+++ program.java
@@ -1,4 +1,3 @@
-package calcapp;
 import java.util.Scanner;
 import java.util.Map;
 import java.util.Arrays;
@@ -12,57 +11,96 @@ import java.util.stream.IntStream;
 public class program {
 	
 
-	static String romeNumArr[]  =  { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX","X"};
+	static String rome[]  =  { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX","X"};
+	static String numbers[]  =  { "1", "2", "3", "4", "5", "6", "7", "8", "9","10"};
 
 	    public static void main(String[] args) {
-	    	//int number1 = 0;
-	    	//int number2 = 0;
-	    	int counter = 0;
+	
 	    	Scanner scanner = new Scanner(System.in);
-	    	System.out.print(">> ");
 	    	String inputString = getInput();
 	    	
-	    	int operator = Operator(inputString);
-	    	String Num1 = num1(inputString);
-	    	String Num2 = num2(inputString);
-	    	
-            System.out.println(operator);
-            System.out.println(Num1);
-            System.out.println(Num2);
-            
-            
-            
-	    	
-	}
-	    
-	    
-	    
-	    
+	    	int Num1 = 0;
+	    	int Num2 = 0;
+	    	int answer = 0;
+	    	String string1 = "0";
+	    	String string2 = "0";
+	        int operator = Operator(inputString);
+	        
+	        //проверяем отсутствие буквенных символов 
+	        if(noString(inputString)) {
+	        	System.out.println("Введите римские или арабские цыфры >>");
+	        }
+	        
+	        //проверка оператора делим строку
+	       	if(operator > 0) {
+		       	string1 = num1(inputString);
+		       	string2 = num2(inputString);
+	       	}else {
+	       		System.out.println("Введите оператор >>");
+	       	}
+	       	//проверка 0 при вводе
+	       	if(string1 == "0" & string2 == "0"){
+	       		System.out.println("Число не меньше 1 >> ");
+	       	}
+	       	 	
+            //проверка ввода римских чисел
+            if(Arrays.asList(rome).contains(string1) & Arrays.asList(rome).contains(string2)) {
+            	answer = calculate(romanToNumber(string1), romanToNumber(string2), operator);
+            	System.out.print(RomanNumerals(answer));
+            }
+	       	//проверка ввода арабских чисел
+            if(Arrays.asList(numbers).contains(string1) & Arrays.asList(numbers).contains(string2)){
+            	answer = calculate(Integer.parseInt(string1), Integer.parseInt(string2), operator);
+            	System.out.print(answer);
+            }
+        
+	       	if(Integer.parseInt(string1) > 10 & Integer.parseInt(string2) > 10){
+	       		System.out.println("Число не больше 10 >> ");
+	       	}
+
+	       	
+	        
+	        
+}    	
+	    //Проверка ввода
+	    public static boolean noString(String inputString) {
+	        return inputString.matches("[a-zA-Z]+");
+	    }
+	
 	    //Ввод
 	    public static String getInput() {
+	    	System.out.print(": ");
 	    	try (Scanner scanner = new Scanner(System.in)) {
 	    		return scanner.next();
 	        }
+	    }
+	   
+	   //Калькулятор
+	    public static int calculate(int number1, int number2, int operation){
+	         int result = 0;
+	         switch (operation){
+	                case 1: result = number1 + number2; break;
+	                case 2: result = number1 - number2; break;
+	                case 3: result = number1 * number2; break;
+	                case 4: result = number1 / number2; break;
+	                default:throw  new IllegalArgumentException("Не верный знак операции");
+	         }
+	         return result;
 	    }
 	    
 	    //оператор
 	    public static int Operator(String inputString) {
 
-	    	int index1 = inputString.indexOf('+');
-	    	int index2 = inputString.indexOf('-');
-	    	int index3 = inputString.indexOf('*');
-	    	int index4 = inputString.indexOf('/');
-	    	
-	    	if(index1 > 0) {
+	    	if(inputString.indexOf('+') != -1) {
 	    		return 1;
 	    	}
-	    	else if(index2 > 0) {
+	    	else if(inputString.indexOf('-') != -1) {
 	    		return 2;
 	    	}
-	    	else if(index3 > 0) {
+	    	else if(inputString.indexOf('*') != -1) {
 	    		return 3;
 	    	}
-	    	else if(index4 > 0) {
+	    	else if(inputString.indexOf('/') != -1) {
 	    		return 4;
 	    	}else {
 	    		return -1;
@@ -71,8 +109,7 @@ public class program {
 	    	
 	    }
 	    
-	    
-	    //индекс оператора в строке
+	    //индекс оператора в строке для разбития строки на подстроки.
 	    public static int OperatorIndex(String inputString) {
 
 	    	int index1 = inputString.indexOf('+');
@@ -80,38 +117,37 @@ public class program {
 	    	int index3 = inputString.indexOf('*');
 	    	int index4 = inputString.indexOf('/');
 	    	
-	     	if(index1 > 0) {
+	     	if(inputString.indexOf('+') != -1) {
 	    		return index1;
 	    	}
-	    	else if(index2 > 0) {
+	    	else if(inputString.indexOf('-') != -1) {
 	    		return index2;
 	    	}
-	    	else if(index3 > 0) {
+	    	else if(inputString.indexOf('*') != -1) {
 	    		return index3;
 	    	}
-	    	else if(index4 > 0) {
+	    	else if(inputString.indexOf('/') != -1) {
 	    		return index4;
 	    	}else {
 	    		return -1;
 	    	}
 	    	
 	    }
-	    //Строка 1
+	    //Строка 1 по опреатору
 	    public static String num1(String inputString) {
-	    	
 	    	int index = OperatorIndex(inputString);
 	    	String result = inputString.substring(0, index);
 	    	return result;
 	    }
-	    //Строка 2
+	    //Строка 2 по оператору
 	    public static String num2(String inputString) {
-	    	
-	    	int InputStringLength = inputString.length();
+	    	String result = "?";
+	    	//int InputStringLength = inputString.length();
 	    	int index = OperatorIndex(inputString);
-	    	String result = inputString.substring(index+1, inputString.length());
+	    	result = inputString.substring(index+1, inputString.length());
 	    	return result;
 	    }
-	     
+	    //перевод в арабские )
 	    private static int romanToNumber(String roman) {
 	        if (roman.equals("I")) {
 	            return 1;
@@ -139,8 +175,8 @@ public class program {
 	    }
 
 	    
-	    
-		  public static String RomanNumerals(int Int) {
+	    //Загуглено и работает перевод в римские
+		public static String RomanNumerals(int Int) {
 			    LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
 			    roman_numerals.put("M", 1000);
 			    roman_numerals.put("CM", 900);
@@ -178,12 +214,3 @@ public class program {
 		  
 		  
 	    }
-
-	   
-	   
-
-	    
-	    
-	 
-
-	   
